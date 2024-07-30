c4a9ca94a21e045896e9766b0b088a7da1985e7b
fktz94 <facundo.ortiz@live.com>
2024-07-30 11:34:07 +0200

    update expo and layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" />
